e1bad59743172e6a95f4b6a8b122f84ca8c0aa79
linyuanxin599 <2508048676@qq.com>
2026-09-19 14:33:26 +0800

    perf: 优化附近公交站空间查询索引
    - 为 stops.geom::geography 增加 GiST 表达式索引

--- database/create_indexes.sql
+++ database/create_indexes.sql
@@ -1,11 +1,17 @@
 -- 线路空间索引
-CREATE INDEX idx_routes_geom
+CREATE INDEX IF NOT EXISTS idx_routes_geom
     ON routes USING GIST (geom);
 
 -- 站点空间索引
-CREATE INDEX idx_stops_geom
+CREATE INDEX IF NOT EXISTS idx_stops_geom
     ON stops USING GIST (geom);
 
+-- 附近站点查询使用 geom::geography，
+-- 因此为相同表达式建立 GiST 表达式索引
+CREATE INDEX IF NOT EXISTS idx_stops_geom_geography
+    ON public.stops
+        USING GIST ((geom::geography));
+
 -- 便于根据站点反查线路
-CREATE INDEX idx_route_stops_stop_id
+CREATE INDEX IF NOT EXISTS idx_route_stops_stop_id
     ON route_stops (stop_id);
